78b6d6fd94ce354e27eb92ad2876c4b635fe639d
Yun-Chi Hsiao <is0.jimhsiao@gmail.com>
2022-01-21 14:10:33 +0800

    Update: Test passed

--- collections_practice.rb
+++ collections_practice.rb
@@ -22,36 +22,31 @@ def reverse_array(array)
 end
 
 def kesha_maker(array)
-array.each do |item|
-	item[2] = "$"
-end
+	array.each do |item|
+		item[2] = "$"
+	end
 end
 
 def find_a(array)
-array.find_all do |word|
-	word[0] == "a"
-end
-
-# using select method
-	# array.select do |word|
-	#   word[0] == "a"
-	# end
+	array.find_all do |word|
+		word[0] == "a"
+	end
 end
 
 def sum_array(array)
-sum = 0
-array.each do |num|
-	sum+=num
-end
-sum
+	sum = 0	
+	array.each do |num|
+		sum+=num
+	end
+	sum
 end
 
 def add_s(array)
-array.collect do |word|
-	if array[1] == word
-	word
-	else
-	word + "s"
+	array.collect do |word|
+		if array[1] == word
+			word
+		else
+			word + "s"
+		end
 	end
-end
 end
